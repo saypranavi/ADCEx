abedf3a8c931ab33b3f8ea748a97a0ea08d39a0f
Pranavi Sayyaparaju <91503364+saypranavi@users.noreply.github.com>
2021-10-13 03:53:28 +0000

    self

--- joystickclass.py
+++ joystickclass.py
@@ -22,7 +22,7 @@ class PCF8591:
 class Joystick:
 
   def _init_(self, address):
-    self.pcf = PCF8591(address)
+    self.pcf = PCF8591(self,address)
     self.bus = smbus.SMBus(1)
     self.address = address
 
